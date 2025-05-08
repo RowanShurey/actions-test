6c501c901ab58f86120882d0ffbd5999e89ea3fe
Rowan Shurey <rowan.shurey@student.unimelb.edu.au>
2025-05-09 08:14:42 +1000

    Update with changes to testing infrastructure and add testing to PR action

--- app/doc-anonymisation/jest.config.js
+++ app/doc-anonymisation/jest.config.js
@@ -1,8 +1,12 @@
 
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-    testEnvironment: "node",
-    transform: {
-      "^.+\.tsx?$": ["ts-jest",{}],
-    },
-  };
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\.tsx?$": ["ts-jest",{}],
+  },
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+
+};
